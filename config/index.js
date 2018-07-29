2ecb8c6231b3038dcabd290b675404775c069cda
Siddharth <kaxxxxxxxxxxxxth@xxxx.xxx>
2018-07-29 18:29:54 +0530

    Clean configuration

--- config/index.js
+++ config/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 
 module.exports = {
-    googleTimeApiKey: process.env.GOOGLE_TIME_API_KEY,
-    googleGeoApiKey: process.env.GOOGLE_GEO_API_KEY
+    googleApiKey: process.env.GOOGLE_API_KEY
 }
--- server/service.js
+++ server/service.js
@@ -9,7 +9,7 @@ const moment = require('moment');
 
 service.get('/service/:location', (req, res, next) => {
 
-    request.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.params.location + '&key=' + config.googleGeoApiKey, (err, response) => {
+    request.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.params.location + '&key=' + config.googleApiKey, (err, response) => {
         if(err) {
             console.log(err);
             return res.sendStatus(500);
@@ -18,7 +18,7 @@ service.get('/service/:location', (req, res, next) => {
         const location = response.body.results[0].geometry.location;
         const timestamp = +moment().format('X');
 
-        request.get('https://maps.googleapis.com/maps/api/timezone/json?location=' + location.lat + ',' + location.lng + '&timestamp=' + timestamp + '&key=' + config.googleTimeApiKey, (err, response) => {
+        request.get('https://maps.googleapis.com/maps/api/timezone/json?location=' + location.lat + ',' + location.lng + '&timestamp=' + timestamp + '&key=' + config.googleApiKey, (err, response) => {
             if(err) {
                 console.log(err);
                 return res.sendStatus(500);
